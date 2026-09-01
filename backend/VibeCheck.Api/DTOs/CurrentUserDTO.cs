f66ebd4546cb02cd0edbca55876b2f3299963196
LudvigSHub <Ludvig.salovuo@gmail.com>
2026-09-01 13:47:32 +0200

    Lagt till AuthMe endpoint plus DTO

---
+++ backend/VibeCheck.Api/DTOs/CurrentUserDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace VibeCheck.Api.DTOs
+{
+    public class CurrentUserDTO
+    {
+        public string UserName { get; set; } = string.Empty;
+        public IEnumerable<string> Roles { get; set; } = [];
+    }
+}
